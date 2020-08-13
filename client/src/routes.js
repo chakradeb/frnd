c31270a7396ada46bb2a2691379c4792fb8e7652
Debarun Chakraborty <debarun.chakraborty96@gmail.com>
2020-08-13 12:39:45 +0530

    Added page not found

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from "./pages/home";
+import Page404 from "./pages/page404";
 
 export default (
     <Switch>
-        <Route path="/"> <Home/> </Route>
+        <Route path="/" exact> <Home/> </Route>
+        <Route path="*"> <Page404 /> </Route>
     </Switch>
 );
